b6f6e75e777dabe72157df0eb78b8dfb4f7c94a5
pierre boue <pierre.boue@univ-grenoble-alpes.fr>
2019-07-03 17:07:35 +0200

    master.sh_update

--- v1.0/example_events/master.sh
+++ v1.0/example_events/master.sh
@@ -5,8 +5,14 @@ python 01_config_download.py
 python 02_download_data.py
 python 03_preprocess_noise.py
 
-for i in `seq 1 2`;
+for i in `seq 1 ${1}`;
 do
         python 04_xcorr.py > stdout_04_${i}.txt &
         echo $i
+        pids[${i}]=$!
 done
+# wait for all pids
+for pid in ${pids[*]}; do
+    wait $pid
+done
+echo "done"
