b68c81d9195a59ea63e60a3cd96590c34cebe84e
Laurent Stehly laurent.stehly@univ-grenoble-alpes.fr <stehlyl@luke.u-ga.fr>
2019-07-16 13:42:35 +0200

    xx

--- v1.0/m_pycorr/m20_noise_processing.py
+++ v1.0/m_pycorr/m20_noise_processing.py
@@ -153,12 +153,11 @@ def _main_loop(in_dir,out_dir,flist,in_) :
                     #print(evt_db['cat'])
 
             # open the input & output file, and scann all metadata and waveforms :
-            ff   = h5py.File(ih5,'r')
-            fout = h5py.File(out_h5,'w') 
-            #except :
-            #    dd.dispc('')
-            #    os.remove(out_h5_lock)
-            #    continue 
+            try :
+                ff   = h5py.File(ih5,'r')
+                fout = h5py.File(out_h5,'w') 
+            except : 
+                continue
 
             if '/_metadata' in ff: fout.copy(ff['/_metadata'],'/_metadata')
             #determining the sampling rate from _metadata
@@ -173,24 +172,42 @@ def _main_loop(in_dir,out_dir,flist,in_) :
                 numfct_r += 1
                 fout = add_metadata(fout,'_metadata/' + '_%02d' % float(numfct_r) + fh.__name__,in_['args'][numfct - 1])
 
-            net_list = ff.items()
+            net_list = ff.keys()
             for inet in net_list :
-                if inet[0] not in ['_metadata']:
-                    fout.create_group(inet[0])
-                    sta_list=inet[1].items()
+                if inet not in ['_metadata']:
+                    fout.create_group(inet)
+                    # test d'integrite 1 
+                    try :
+                        sta_list = ff[inet].keys()
+                        len(sta_list)
+                    except :
+                        continue 
                     for ista in sta_list :
-                        group_name='/'+inet[0]+'/'+ista[0]
+                        group_name='/'+inet+'/'+ista
+                        print(group_name)
                         fout.create_group(group_name)
-                        #dd.dispc('   '+ista[0],'c','b')
-                        for ich in ista[1].items() :
-
+                        # integrity check
+                        try :
+                            for ich in ff[group_name] :
+                                pass
+                        except :
+                            continue 
+                        
+                        for ich in ff[group_name]  : 
+                        #for ich in ista[1].items() :
                             # get fe init for each trace
                             if '/_metadata/fe' in ff: fe = ff['/_metadata/fe'][()]
                             else : fe = float(in_dir.split('/')[-1].split('_')[1].split('h')[0])
-                            dataset_name= '/'+inet[0]+'/'+ista[0]+'/'+ich[0]
-                            ff.copy(dataset_name,fout[group_name])
-                            trace = ich[1][:]
-                            
+                            dataset_name= '/'+inet+'/'+ista+'/'+ich
+                            #print('  '+dataset_name)
+                            # integrity check 2 : 
+                            try : 
+                                trace = ff[dataset_name][:] 
+                            except : 
+                                continue 
+                            #print('    got trace')
+                            fout.create_dataset(dataset_name,data = trace)
+                            #print('    created dataset')
                             segment      = False
                             method_index = 0
                             for fh in flist:
@@ -257,10 +274,6 @@ def _main_loop(in_dir,out_dir,flist,in_) :
                                     fout.create_dataset(dataset_name,data = trace)
                                 else:
                                     fout[dataset_name][:] = trace
-
-
-
-
                             
             #cleaning
             fout.close()
