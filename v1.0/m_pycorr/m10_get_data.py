63874a907f1628049fea3a0eb0630715ad80376a
pierre boue <pierre.boue@univ-grenoble-alpes.fr>
2021-11-19 13:48:53 +0100

    modif get data

--- v1.0/m_pycorr/m10_get_data.py
+++ v1.0/m_pycorr/m10_get_data.py
@@ -596,8 +596,8 @@ def load_from_source(queue,in_,out_file,client,date1,date2,ista,icmp) :
             else : 
                 st = read_SDS(ista['net'],ista['name'],ista['loc'],ista['ch']+icmp,date1-tap_len,date2+tap_len,archive_path,meta_path,tap_len,remove_resp)
         else :
-            dd.dd(ista)
-            print(ista['net'] + '  ' + ista['name'] + '  ' + ista['loc'] + '  ' + ccmp + '  ' + date1 + '  ' + date2)
+            #dd.dd(ista)
+            print(ista['net'] + '  ' + ista['name'] + ' --- ' + date1 + '  ' + date2)
             if 'ch' not in ista :
                 dd.dispc('      attempting to find ch (WEB)','c','d')
                 st,ista = determine_ch_and_locid_from_client(ista,icmp,in_['ch'],client,date1-tap_len,date2+tap_len)
